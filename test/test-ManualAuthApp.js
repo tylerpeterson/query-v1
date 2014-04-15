ae84ff487cece984c6c2e5d9d0a3d44ce972e8ec
Tyler Peterson <petersontb@familysearch.org>
2014-04-14 18:22:29 -0600

    Read refresh token from disk
    Test happy day of reading token from disk.
    Implement to pass the test.

--- test/test-ManualAuthApp.js
+++ test/test-ManualAuthApp.js
@@ -1,15 +1,43 @@
 var expect = require('chai').expect;
+var sinon = require('sinon');
+var fs = require('fs');
 var ManualAuthApp = require('../lib/ManualAuthApp');
 var app = require('express')();
 var secrets = {web: {}};
 
 describe('ManualAuthApp', function () {
+  var instance;
+
+  beforeEach(function () {
+    instance = new ManualAuthApp(secrets, {app: app});
+  });
+
   it('should be a constructor', function () {
-    var instance = new ManualAuthApp(secrets, {app: app});
     expect(instance).to.be.an.instanceof(ManualAuthApp);
   });
 
-  it.skip('should read cached tokens', function (done) {
+  describe('caching on disk', function () {
+    var readFile;
 
+    beforeEach(function () {
+      readFile = sinon.stub(fs, 'readFile', function (path, cb) {
+        cb(null, '{"refreshToken":"access_token_on_disk"}');
+      });
+    });
+
+    it('should read cached refresh token', function (done) {
+      instance.refreshTokenPromise().then(function (refreshToken) {
+        try {
+          expect(refreshToken).to.equal('access_token_on_disk');
+          done();
+        } catch (err) {
+          done(err);
+        }
+      }, done);
+    });
+
+    afterEach(function () {
+      readFile.restore();
+    });
   });
 });
