afd4e27e940a63e0196d320bb70cd8372e33051c
Tyler Peterson <petersontb@familysearch.org>
2014-04-16 20:37:34 -0600

    Sketch out strategy pattern.
    Not a lot of time today.  Only time to sketch out a design.

---
+++ test/test-RefreshTokenMissedStrategy.js
@@ -0,0 +1,19 @@
+// TODO move to a component test. This test will be slow.
+
+describe('RefreshTokenMissedStrategy', function () {
+  describe.skip('LaunchBrowser version', function () {
+    before(function () {
+      // TODO launch an app to detect the browers connecting
+    });
+
+    it('should open a browser to the flow start url', function () {
+      // TODO trigger the strategy
+      // TODO verify that the test app detected the connection.
+    });
+
+    after(function () {
+      // TODO kill the app you launched
+      // TODO kill the browser you launched
+    })
+  });
+});
