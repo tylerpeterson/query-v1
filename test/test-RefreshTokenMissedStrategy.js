41db0dbdcb52a7cb2fd2a9a4cadc9466a5ea00b9
Tyler Peterson <petersontb@familysearch.org>
2014-04-17 19:06:53 -0600

    Implement reject promise strategy
    Implemented the easier reject promise strategy.

--- test/test-RefreshTokenMissedStrategy.js
+++ test/test-RefreshTokenMissedStrategy.js
@@ -1,6 +1,19 @@
+var expect = require('chai').expect;
+var Q = require('q');
+var strategies = require('../lib/RefreshTokenMissedStrategy');
+var rejectPromise = strategies.rejectPromise;
+
 // TODO move to a component test. This test will be slow.
 
 describe('RefreshTokenMissedStrategy', function () {
+  var refreshTokenDfd;
+  var refreshTokenPromise;
+
+  beforeEach(function () {
+    refreshTokenDfd = Q.defer();
+    refreshTokenPromise = refreshTokenDfd.promise;
+  });
+
   describe.skip('LaunchBrowser version', function () {
     before(function () {
       // TODO launch an app to detect the browers connecting
@@ -14,6 +27,13 @@ describe('RefreshTokenMissedStrategy', function () {
     after(function () {
       // TODO kill the app you launched
       // TODO kill the browser you launched
-    })
+    });
+  });
+
+  describe('RejectPromise version', function () {
+    it('should reject the promise for the refresh token', function () {
+      rejectPromise(refreshTokenDfd);
+      expect(refreshTokenPromise.isRejected());
+    });
   });
 });
