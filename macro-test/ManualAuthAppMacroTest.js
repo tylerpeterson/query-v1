eea94b5dcc43b878905d52114cb6844ba29cfb59
Tyler Peterson <petersontb@familysearch.org>
2014-04-22 19:52:31 -0600

    Start non-unit tests.
    Unit tests can’t touch disk, and certainly shouldn’t make network
    requests.  Yet, calling the actual flow seems the most straightforward
    way to know I haven’t broken it.  Creating integration-like tests.
    Macro tests, opposed to micro tests a la industrial logic TDD training.

---
+++ macro-test/ManualAuthAppMacroTest.js
@@ -0,0 +1,12 @@
+describe('ManualAuthApp', function () {
+  it.skip('should read cached auth tokens', function () {
+    /* TODO
+     * generate a temp directory and use it as the cache base
+     * poke tokens into the temp dir
+     * get a promise for access token and then refresh token
+     * verify they match the poked data.
+     *
+     * Possibly two tests, not one.
+     */
+  });
+});
