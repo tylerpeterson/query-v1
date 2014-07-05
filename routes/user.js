58d9526e7557ecc577588ff9d3b9aa3a08a1e5fc
Tyler Peterson <petersontb@familysearch.org>
2014-07-04 19:22:22 -0600

    Read users from v1.

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,28 @@
+var request = require('superagent');
+var secrets = require('../client_secrets'); // TODO shouldn't have to require this again, already in v1oauth
+var serverBaseUri = secrets.web.server_base_uri; // TODO shouldn't have to look this up. Use v1oauth.
 
 /*
  * GET users listing.
  */
 
 exports.list = function(req, res){
-  res.send("respond with a resource");
+  var query = {
+    from: 'Member',
+    select: [
+      'Name', 'Nickname'
+    ]
+  };
+  var token = req.cookies.v1accessToken; // TODO shouldn't read off of the cookie, let v1oauth know that stuff.  
+  request
+    .get(serverBaseUri + '/query.v1') // TODO v1oauth should help with this url
+    .set('Authorization', 'Bearer ' + token) // TODO v1oauth should help with setting this header
+    .send(query)
+    .end(function (queryRes) {
+      if (queryRes.ok) {
+        res.send('success!\n' + JSON.stringify(queryRes.body, null, 2));
+      } else {
+        res.send('failure :-(\n' + queryRes.text);
+      }
+    });
 };
