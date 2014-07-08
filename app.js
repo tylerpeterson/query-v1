cd20c52032f7403099db1b65bdc4dd2dbe0bbc46
Tyler Peterson <petersontb@familysearch.org>
2014-07-07 20:31:33 -0600

    Port to express 4

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ var url = require('url');
 var secrets = require('./client_secrets');
 var AuthApp = require('v1oauth').app;
 var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var methodOverride = require('method-override');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var errorHandler = require('errorhandler');
 
 var app = express();
 var port = url.parse(secrets.web.redirect_uris[0]).port;
@@ -28,26 +34,26 @@ function appBaseFromSecrets(secrets) {
 app.set('port', port);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(bodyParser());
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(methodOverride());
+app.use(cookieParser('your secret here'));
+app.use(session());
+
+app.get('/', routes.index);
+app.get('/users', auth.restrict, user.list);
+app.post('/users', auth.restrict, user.postList);
+
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(auth.router);
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+  app.use(errorHandler());
 }
-
-app.get('/', routes.index);
-app.get('/users', auth.restrict, user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
