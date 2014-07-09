2b7554c912245a7d7611b1f35cb4dbd6986ebc38
Tyler Peterson <petersontb@familysearch.org>
2014-07-08 20:19:01 -0600

    Record check-boxes on post

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,17 @@
 var request = require('superagent');
 var secrets = require('../client_secrets'); // TODO shouldn't have to require this again, already in v1oauth
 var serverBaseUri = secrets.web.server_base_uri; // TODO shouldn't have to look this up. Use v1oauth.
+var DataStore = require('nedb');
+var path = require('path');
+var users = new DataStore({
+  filename: path.join(__dirname, '..', 'cache', 'users.nedb'),
+  autoload: true,
+  onload: function (err) {
+    if (err) console.log('error loading user database', err);
+  }
+});
+var Q = require('q');
+var debug = require('debug')('query-v1');
 
 /*
  * GET users listing.
@@ -20,6 +31,7 @@ exports.list = function(req, res){
     .send(query)
     .end(function (queryRes) {
       if (queryRes.ok) {
+        // TODO read flagged users and set their check-boxes
         res.render('users', { title: 'All Users', users: queryRes.body[0]});
       } else {
         res.send('failure :-(\n' + queryRes.text);
@@ -28,7 +40,13 @@ exports.list = function(req, res){
 };
 
 exports.postList = function (req, res) {
-  // TODO persist selection
-  console.log(req.body);
-  res.redirect('/users');
+  // TODO unflag those flagged in the DB and not in this post.
+  var upsertPromises = [];
+  req.body.selectedUsers.forEach(function (userOid) {
+    upsertPromises.push(Q.ninvoke(users, "update", {_oid: userOid}, {$set: {flagged: true, _oid: userOid}}, {upsert: true}));
+  });
+  Q.allSettled(upsertPromises).then(function (states) {
+    debug('upsertsDone %s', JSON.stringify(states, null, ' '));
+    res.redirect('/users');
+  });
 };
