995a687af769628e192b71ec5a32b6d7974bfcba
Tyler Peterson <petersontb@familysearch.org>
2014-07-11 17:59:48 -0600

    Begin extracting userService

--- routes/user.js
+++ routes/user.js
@@ -1,37 +1,10 @@
 var request = require('superagent');
 var secrets = require('../client_secrets'); // TODO shouldn't have to require this again, already in v1oauth
 var serverBaseUri = secrets.web.server_base_uri; // TODO shouldn't have to look this up. Use v1oauth.
-var DataStore = require('nedb');
-var path = require('path');
 var Q = require('q');
 var debug = require('debug')('query-v1');
 var _ = require('lodash');
-
-
-/*\
- * Set up data store
-\*/
-
-var users = new DataStore({
-  filename: path.join(__dirname, '..', 'cache', 'users.nedb'),
-  autoload: true,
-  onload: function (err) {
-    if (err) console.log('error loading user database', err);
-  }
-});
-
-function indexReporter(indexName) {
-  return function (err) {
-    if (err) {
-      debug('err adding index %s', indexName, err);
-    } else {
-      debug('success adding index %s', indexName);
-    }
-  };
-}
-
-users.ensureIndex({fieldName: '_oid', unique: true}, indexReporter('_oid'));
-users.ensureIndex({fieldName: 'flagged', unique: false, sparse: true}, indexReporter('flagged'));
+var users = require('../lib/userService').users;
 
 /*
  * GET users listing.
