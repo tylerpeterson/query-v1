ff9c2a82ac15ce5f7e40a7936ab095a0525c4513
Tyler Peterson <petersontb@familysearch.org>
2014-09-23 11:40:45 -0600

    extract v1Query for reuse

--- routes/user.js
+++ routes/user.js
@@ -1,19 +1,8 @@
-var request = require('superagent');
-var secrets = require('../client_secrets'); // TODO shouldn't have to require this again, already in v1oauth
-var serverBaseUri = secrets.web.server_base_uri; // TODO shouldn't have to look this up. Use v1oauth.
 var Q = require('q');
 var debug = require('debug')('query-v1');
 var _ = require('lodash');
 var userService = require('../lib/userService');
-
-function v1Query(req, query) { // TODO shouldn't need the req object
-  var token = req.cookies.v1accessToken; // TODO shouldn't read off of the cookie, let v1oauth know that stuff.  
-
-  return request
-    .get(serverBaseUri + '/query.v1') // TODO v1oauth should help with this url
-    .set('Authorization', 'Bearer ' + token) // TODO v1oauth should help with setting this header
-    .send(query)
-}
+var v1Query = require('../lib/v1Query');
 
 /*
  * GET users listing.
