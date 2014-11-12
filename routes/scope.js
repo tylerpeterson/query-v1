5a78502aba2abf10c6ad78c356ddcf40d96956a1
Tyler Peterson <petersontb@familysearch.org>
2014-11-12 14:28:57 -0700

    Do more querying

--- routes/scope.js
+++ routes/scope.js
@@ -18,15 +18,47 @@ exports.listStoriesForScope = function (req, res, next) {
   debug('scopeId', scopeId);
   debug('query', query);
   v1Query(req, query)
-  .end(function (queryRes) {
-    if (queryRes.ok) {
-      debug(JSON.stringify(queryRes.body, null, ' '));
-      var scopes = queryRes.body[0];
-      res.send('success! :D');
-    } else {
-      res.send('failure :-(\n' + queryRes.text);
-    }
-  });
+    .end(function (queryRes) {
+      if (queryRes.ok) {
+        debug(JSON.stringify(queryRes.body, null, ' '));
+        var scopes = queryRes.body[0];
+        var query = scopes.map(function (scope) {
+          return {
+            from: "Story",
+            select: [
+              "Name",
+              "Estimate",
+              "Order",
+              "Team.Name",
+              "Team",
+              "Status.Name",
+              "Timebox.Name",
+              "Timebox.EndDate",
+              "Timebox"
+            ],
+            where: {
+              "Scope.ID": scope._oid
+            }
+          };
+        });
+        debug('second query', query);
+        v1Query(req, query).end(function (queryRes) {
+          if (queryRes.ok) {
+            var stories = _.flatten(queryRes.body, true);
+            var backlogs = _.groupBy(stories, function (story) {
+              return story.Team._oid;
+            });
+            debug(JSON.stringify(Object.keys(backlogs), null, ' '));
+
+            res.send('success! :D');
+          } else {
+            res.send('failure (2) :-(\n' + queryRes.text);
+          }
+        });
+      } else {
+        res.send('failure :-(\n' + queryRes.text);
+      }
+    });
 }
 
 exports.getRouter = function (v1Auth) {
